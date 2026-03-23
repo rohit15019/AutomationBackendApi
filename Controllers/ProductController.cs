7fc9db0f191159f440f408efc807b756f0a1b96c
Devang <147418262+Devanghadiyal11@users.noreply.github.com>
2026-03-23 18:13:14 +0530

    add method

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -7,6 +7,31 @@ namespace AutomationBackend.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-
-    }
+        // GET: api/Product
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        // GET: api/Product/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        // POST: api/Product
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+        // PUT: api/Product/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+        // DELETE: api/Product/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
 }
