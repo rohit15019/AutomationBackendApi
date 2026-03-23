b495b2041c371481d095b4e94a1dfdd990846442
Devang <147418262+Devanghadiyal11@users.noreply.github.com>
2026-03-23 17:48:03 +0530

    Old Controller Delete and New Added

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutomationBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = new List<string> { "Product 1", "Product 2", "Product 3" };
+            return Ok(products);
+        }
+    }
+}
